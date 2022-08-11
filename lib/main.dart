e2490cb0015cb0db7622ea7c62014983587b2c64
developer smart <developersmartvision@gmail.com>
2022-08-11 15:16:19 +0700

    Card and Parsing

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Dashboard",
+    title: "Card & Parsing",
     home: HalamanSatu(),
   ));
 }
@@ -11,24 +11,45 @@ class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black12,
       appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        leading: Icon(Icons.home, color: Colors.orange),
-        title: Center(child: Text("Dashboard")),
-        actions:[Icon(Icons.search)],
-      ),
+          title: Text("Card & Parsing"),
+          backgroundColor: Colors.lightGreenAccent),
       body: Container(
-        child: Row(
-          children:[
-            Icon(Icons.local_parking, size: 70.0, color: Colors.redAccent),
-            Column(children: [
-              Icon(Icons.local_parking, size: 70.0, color: Colors.blue),
-              Icon(Icons.local_parking, size: 70.0, color: Colors.blue),
-              Icon(Icons.local_parking, size: 70.0, color: Colors.blue)
-            ],),
-            Icon(Icons.local_parking, size: 70.0, color: Colors.blue)
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            CardSaya(
+                icons: Icons.home, texts: "Home", warna: Colors.blueAccent),
+            CardSaya(
+                icons: Icons.local_parking, texts: "Location", warna: Colors.blueAccent),
+            CardSaya(icons: Icons.supervised_user_circle, texts: "Juru Parkir", warna: Colors.blueAccent)
+          ],
+        ),
+      ),
+    );
+  }
+}
 
+class CardSaya extends StatelessWidget {
+  CardSaya({required this.icons, required this.texts, required this.warna});
+
+  final IconData icons;
+  final String texts;
+  final Color warna;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      child: Card(
+        color: Colors.redAccent,
+        child: Column(
+          children: [
+            Icon(icons, size: 50.0, color: warna),
+            Text(
+              texts,
+              style: TextStyle(fontSize: 20.0, color: Colors.white),
+            )
           ],
         ),
       ),
