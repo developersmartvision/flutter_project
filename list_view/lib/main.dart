9bc3d687d716ec8c28f34ac1f6781f3dcb8f9625
developer smart <developersmartvision@gmail.com>
2022-08-12 13:56:23 +0700

    list view json

--- list_view/lib/main.dart
+++ list_view/lib/main.dart
@@ -2,85 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "list View",
-    home: Home(),
+    title: "Json Data Listview",
+    home: Home(data: List<String>.generate(300, (i) => "ini data ke $i"),),
   ));
 }
 
 class Home extends StatelessWidget {
+  final List<String> data;
+
+  Home({required this.data});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueAccent[800],
-        title: Text("Daftar produk"),
+        title: Text("List View"),
       ),
-      body: ListView(
-        children: [
-          ListTutorial(
-            gambar:
-                "https://ds393qgzrxwzn.cloudfront.net/resize/m500x500/cat1/img/images/0/O4IkgkAMTL.jpg",
-            judul: "Speaker",
-          ),
-          ListTutorial(
-            gambar:
-                "https://ds393qgzrxwzn.cloudfront.net/resize/m500x500/cat1/img/images/0/O4IkgkAMTL.jpg",
-            judul: "Speaker1",
-          ),
-          ListTutorial(
-            gambar:
-                "https://ds393qgzrxwzn.cloudfront.net/resize/m500x500/cat1/img/images/0/O4IkgkAMTL.jpg",
-            judul: "Speaker2",
-          ),
-          ListTutorial(
-            gambar:
-                "https://ds393qgzrxwzn.cloudfront.net/resize/m500x500/cat1/img/images/0/O4IkgkAMTL.jpg",
-            judul: "Speaker3",
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class ListTutorial extends StatelessWidget {
-  // const ListTutorial({Key? key}) : super(key: key);
-  ListTutorial({required this.gambar, required this.judul});
-
-  final String gambar;
-  final String judul;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(2.0),
-      child: Center(
-        child: Row(
-          children: [
-            Image(
-              image: NetworkImage(gambar),
-              width: 100.0,
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Center(
-                child: Column(
-                  children: [
-                    Text(
-                      judul,
-                      style: TextStyle(fontSize: 20.0, wordSpacing: 10.0),
-                    ),
-                    Text(
-                      "Deskripsi",
-                      style: TextStyle(
-                        fontSize: 15.0,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
+      body: Container(
+        child: ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, index){
+            return ListTile(
+              leading: Icon (Icons.widgets),
+              title: Text("${data[index]}"),
+            );
+          },
         ),
       ),
     );
